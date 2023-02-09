25887d158857e22f89b3330f421a23a5b5ef37d6
Vinh Tran <106710730+vtran0314@users.noreply.github.com>
2023-02-09 17:42:00 -0500

    Create About-Python.py

---
+++ Week1/About-Python.py
@@ -0,0 +1,7 @@
+###Creator: Vinh Tran
+###
+print("Python Creator: Guido van Rossum")
+
+print("Python was created in 1999, every project start with a goal ")
+
+print("BDFL means: Benevolent dictator for life")
